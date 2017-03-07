719745aad603c0bc8f5898a3c00134edc385b8a2
miliang <2318423730@qq.com>
2017-03-07 17:07:42 +0800

    2

--- app/src/main/java/com/eollse/activity/MainActivity.java
+++ app/src/main/java/com/eollse/activity/MainActivity.java
@@ -2,19 +2,17 @@ package com.eollse.activity;
 
 
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
-import android.util.Log;
 
 
 import com.eollse.R;
+
 import com.eollse.ui.MyPmdTextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends BaseActivity {
-    public static final int HANDLER_TXT_SCROLL_OVER = 1;//一串文字滚动完成(跑马灯)
+
     private MyPmdTextView myPmdTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,44 +22,26 @@ public class MainActivity extends BaseActivity {
         myPmdTextView= (MyPmdTextView) findViewById(R.id.tv_pmd);
         
         setPmdData();
-//        Log.e("MyTAG", "获取高度:"+myPmdTextView.getMeasuredWidth());
-//        if(myPmdTextView.getWidth()>0){
-//            Log.e("MyTAG", "获取高度:"+myPmdTextView.getWidth());
-            myPmdTextView.setHandler(mHandler);
-            myPmdTextView.setStr(pmdList.get(0));
- //       }
 
 
-
+        for(int i=0;i<pmdList.size();i++){
+            str+=(pmdList.get(i)+"\t\t\t\t\t\t\t\t\t\t");
+        }
+        myPmdTextView.setText(str);
 
     }
+    private String str="";
     private List<String> pmdList;
     private void setPmdData() {
         pmdList=new ArrayList<>();
-        pmdList.add("第一组");
-        pmdList.add("第二组");
-        pmdList.add("第三组");
-        pmdList.add("第四组");
-        pmdList.add("第五组");
+        pmdList.add("第一组aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        pmdList.add("第二组bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb");
+        pmdList.add("第三组cccccccccccccccccccccccccccccccccccccccccccccccc");
+        pmdList.add("第四组ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd");
+        pmdList.add("第五组eeeeeeeeeeeeeeeeeeeeeeeeeeeeeee");
 
     }
 
-    private int i=1;
-    private Handler mHandler=new Handler(){
-        @Override
-        public void handleMessage(Message msg) {
-           switch (msg.what){
-               case HANDLER_TXT_SCROLL_OVER:
-                   myPmdTextView.setStr(pmdList.get(i));
-                   Log.e("MyTAG","换另一组文字滚动");
-                   i++;
-                   if(i==pmdList.size()){
-                       i=0;
-                   }
-                   break;
-           }
-        }
-    };
 
     
 }
--- app/src/main/java/com/eollse/ui/MyPmdTextView.java
+++ app/src/main/java/com/eollse/ui/MyPmdTextView.java
@@ -1,112 +1,26 @@
 package com.eollse.ui;
 
 import android.content.Context;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.os.Handler;
-
 import android.util.AttributeSet;
-import android.util.Log;
 import android.widget.TextView;
-
-import com.eollse.activity.MainActivity;
 
 /**
  * Created by miliang on 2017/3/7/0007.
  */
 
 public class MyPmdTextView extends TextView {
-    Paint mpaint = new Paint();
-    private String str;//文字
-    private int textWidth;//文字宽度
-    private boolean isOver = false;//文字跑马灯完成
-    private int myTextViewWidth;//控件宽度
-    private int pointX, pointY = 20;
-
     public MyPmdTextView(Context context) {
-        super(context, null);
-
+        super(context,null);
     }
 
     public MyPmdTextView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        mpaint.setColor(Color.RED);
-        mpaint.setAntiAlias(true);
-        mpaint.setStyle(Paint.Style.STROKE);
-        mpaint.setTextSize(20);
     }
-
-
-
-
+    public MyPmdTextView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
     @Override
-    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-
-        myTextViewWidth = w;
-        pointY = h;
-        Log.e("MyTAG", "onSizeChanged控件宽度" + w);
-        isOver = false;
-        textWidth = (int) mpaint.measureText(str);
-        Log.e("MyTAG", "文字宽度" + textWidth);
-
-        pointX = myTextViewWidth;
-        Log.e("MyTAG", "x坐标被赋值" + pointX);
-        setStr(str,0);
-    }
-
-    @Override
-    public void draw(Canvas canvas) {
-
-        canvas.drawText(str, pointX, pointY / 2, mpaint);
-        super.draw(canvas);
-    }
-
-    /**
-     * 设置文字
-     *
-     * @param string
-     */
-    public void setStr(String string) {
-        str = string;
-        if(pointX>0){
-            isOver=false;
-            pointX = myTextViewWidth;
-            pmdStart();
-        }
-    }
-    public void setStr(String string,int i) {
-        pmdStart();
-
-    }
-    private void pmdStart(){
-        myThread = new MyThread();
-        myThread.start();
-    }
-
-    public void setHandler(Handler handler) {
-        this.handler = handler;
-    }
-
-    private Handler handler;
-    private MyThread myThread;
-
-    private class MyThread extends Thread {
-        @Override
-        public void run() {
-            if (!isOver) {
-                pointX -= 2;
-            }
-            if (pointX < -textWidth) {
-                isOver = true;
-                Log.e("MyTAG", "完成");
-                pointX = myTextViewWidth;
-                handler.sendEmptyMessage(MainActivity.HANDLER_TXT_SCROLL_OVER);
-
-            }
-            postInvalidate();
-
-            postDelayed(this, 20);
-        }
+    public boolean isFocused() {
+        return true;
     }
 }
