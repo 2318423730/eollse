95615486512c2b34fc9f84d6998417e6e0e5122b
miliang <2318423730@qq.com>
2017-03-07 16:34:13 +0800

    Initial commit

---
+++ app/src/main/java/com/eollse/activity/BaseActivity.java
@@ -0,0 +1,15 @@
+package com.eollse.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by miliang on 2017/3/7/0007.
+ */
+
+public class BaseActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/eollse/app/MyApplication.java
@@ -0,0 +1,14 @@
+package com.eollse.app;
+
+import android.app.Application;
+
+/**
+ * Created by miliang on 2017/3/7/0007.
+ */
+
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
