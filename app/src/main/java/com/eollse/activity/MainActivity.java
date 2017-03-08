281c3378200be5cc1047ad029157d1f703f3dd03
miliang <2318423730@qq.com>
2017-03-08 10:31:01 +0800

    2017-3-8  第1次提交

--- app/src/main/java/com/eollse/activity/MainActivity.java
+++ app/src/main/java/com/eollse/activity/MainActivity.java
@@ -2,38 +2,94 @@ package com.eollse.activity;
 
 
 import android.os.Bundle;
-
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.eollse.R;
-
 import com.eollse.ui.MyPmdTextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends BaseActivity {
 
-    private MyPmdTextView myPmdTextView;
+
+    @BindView(R.id.ll_top1)
+    LinearLayout llTop1;
+    @BindView(R.id.iv_setting)
+    ImageView ivSetting;
+    @BindView(R.id.Address)
+    TextView Address;
+    @BindView(R.id.RealName)
+    TextView RealName;
+    @BindView(R.id.Mobile)
+    TextView Mobile;
+    @BindView(R.id.ll_top2)
+    LinearLayout llTop2;
+    @BindView(R.id.rl_top2)
+    RelativeLayout rlTop2;
+    @BindView(R.id.aaa)
+    TextView aaa;
+    @BindView(R.id.lv_listview)
+    ListView lvListview;
+    @BindView(R.id.ll_middle_left)
+    LinearLayout llMiddleLeft;
+    @BindView(R.id.ll_wyhs)
+    LinearLayout llWyhs;
+    @BindView(R.id.ll_zxcc)
+    LinearLayout llZxcc;
+    @BindView(R.id.ll_cxgs)
+    LinearLayout llCxgs;
+    @BindView(R.id.ll_pasq)
+    LinearLayout llPasq;
+    @BindView(R.id.ll_wyfw)
+    LinearLayout llWyfw;
+    @BindView(R.id.ll_hrhs)
+    LinearLayout llHrhs;
+    @BindView(R.id.ll_wybs)
+    LinearLayout llWybs;
+    @BindView(R.id.iv_zwfu)
+    ImageView ivZwfu;
+    @BindView(R.id.iv_shfw)
+    ImageView ivShfw;
+    @BindView(R.id.iv_add)
+    ImageView ivAdd;
+    @BindView(R.id.iv_search)
+    ImageView ivSearch;
+    @BindView(R.id.tv_pmd)
+    MyPmdTextView tvPmd;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        myPmdTextView= (MyPmdTextView) findViewById(R.id.tv_pmd);
-        
+        tvPmd = (MyPmdTextView) findViewById(R.id.tv_pmd);
+
         setPmdData();
 
 
-        for(int i=0;i<pmdList.size();i++){
-            str+=(pmdList.get(i)+"\t\t\t\t\t\t\t\t\t\t");
+        for (int i = 0; i < pmdList.size(); i++) {
+            str += (pmdList.get(i) + "\t\t\t\t\t\t\t\t\t\t");
         }
-        myPmdTextView.setText(str);
+        tvPmd.setText(str);
 
     }
-    private String str="";
+
+    private String str = "";
     private List<String> pmdList;
+
     private void setPmdData() {
-        pmdList=new ArrayList<>();
+        pmdList = new ArrayList<>();
         pmdList.add("第一组aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
         pmdList.add("第二组bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb");
         pmdList.add("第三组cccccccccccccccccccccccccccccccccccccccccccccccc");
@@ -43,5 +99,4 @@ public class MainActivity extends BaseActivity {
     }
 
 
-    
 }
