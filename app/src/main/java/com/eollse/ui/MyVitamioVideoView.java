e3de4d76f51fed10ea7fa91a56c50c95dd998448
miliang <2318423730@qq.com>
2017-03-16 11:09:53 +0800

    2017-3-16  第3次提交

--- app/src/main/java/com/eollse/ui/MyVitamioVideoView.java
+++ app/src/main/java/com/eollse/ui/MyVitamioVideoView.java
@@ -28,8 +28,12 @@ public class MyVitamioVideoView extends VideoView {
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        setMeasuredDimension(widthMeasureSpec,heightMeasureSpec);
+//        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+//        setMeasuredDimension(widthMeasureSpec,heightMeasureSpec);
+        //我们重新计算高度
+        int width = getDefaultSize(0, widthMeasureSpec);
+        int height = getDefaultSize(0, heightMeasureSpec);
+        setMeasuredDimension(width, height);
     }
 
 
