d6e1367ba3cf1152b31ad1f2af67a6c30230474b
miliang <2318423730@qq.com>
2017-03-16 11:55:32 +0800

    2017-3-16  第4次提交

---
+++ app/src/main/java/com/eollse/activity/WelcomeActivity.java
@@ -0,0 +1,33 @@
+package com.eollse.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.eollse.R;
+
+public class WelcomeActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        handler.sendEmptyMessageDelayed(0,2000);
+    }
+    private Handler handler=new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what){
+                case 0:
+                    Intent intent=new Intent(WelcomeActivity.this,MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                    break;
+            }
+        }
+    };
+}
