c7e224047ff0cfb488de49aa2e223fd052c03597
miliang <2318423730@qq.com>
2017-03-17 16:04:41 +0800

    2017-3-17第4次提交 结果查询界面

--- app/src/main/java/com/eollse/activity/JgcxActivity.java
+++ app/src/main/java/com/eollse/activity/JgcxActivity.java
@@ -1,18 +1,53 @@
 package com.eollse.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.eollse.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * 结果查询
  */
 public class JgcxActivity extends BaseActivity {
 
+    @BindView(R.id.tv_backHome)
+    TextView tvBackHome;
+    @BindView(R.id.tv_title)
+    TextView tvTitle;
+    @BindView(R.id.tv_back)
+    TextView tvBack;
+    @BindView(R.id.et_userName)
+    EditText etUserName;
+    @BindView(R.id.et_mobile)
+    EditText etMobile;
+    @BindView(R.id.tv_search)
+    TextView tvSearch;
+    @BindView(R.id.lv_info)
+    ListView lvInfo;
+    @BindView(R.id.tv_noInfo)
+    TextView tvNoInfo;
+    @BindView(R.id.tv_previous)
+    TextView tvPrevious;
+    @BindView(R.id.tv_currentPage)
+    TextView tvCurrentPage;
+    @BindView(R.id.tv_totalPage)
+    TextView tvTotalPage;
+    @BindView(R.id.tv_totalSize)
+    TextView tvTotalSize;
+    @BindView(R.id.tv_next)
+    TextView tvNext;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jgcx);
+        ButterKnife.bind(this);
+
+        tvTitle.setText("结果查询");
     }
 }
