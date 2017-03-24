6aa9b27a7eecde3baf1c6dbbda0d5fb42f3c1900
miliang <2318423730@qq.com>
2017-03-24 10:14:06 +0800

    预约办事

--- app/src/main/java/com/eollse/activity/YybsActivity.java
+++ app/src/main/java/com/eollse/activity/YybsActivity.java
@@ -1,21 +1,27 @@
 package com.eollse.activity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import com.eollse.R;
-import com.eollse.adapter.YybsTimeAdapter2;
+import com.eollse.adapter.YybsTypeAdapter;
 import com.eollse.app.MyApplication;
 import com.eollse.utils.Constants;
 import com.eollse.utils.KCalendar;
+import com.eollse.utils.MyToast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -61,11 +67,26 @@ public class YybsActivity extends BaseActivity implements CompoundButton.OnCheck
     RadioButton rbTime22;
     @BindView(R.id.rb_time2_3)
     RadioButton rbTime23;
+    @BindView(R.id.lv_type)
+    ListView lvType;
+    @BindView(R.id.et_userName)
+    EditText etUserName;
+    @BindView(R.id.et_phone)
+    EditText etPhone;
+    @BindView(R.id.et_code)
+    EditText etCode;
+    @BindView(R.id.btn_commit)
+    Button btnCommit;
+    @BindView(R.id.iv_more)
+    ImageView ivMore;
 
 
     private String date = null;// 设置默认选中的日期  格式为 “2014-04-05” 标准DATE格式
-    private YybsTimeAdapter2 yybsTimeAdapter;
-    private List<String> timeList = new ArrayList<>();
+
+
+    private String time;//时间段
+    private List<String> typeList = new ArrayList<>();
+    private YybsTypeAdapter yybsTypeAdapter;
     private Handler handler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
@@ -85,10 +106,25 @@ public class YybsActivity extends BaseActivity implements CompoundButton.OnCheck
         ButterKnife.bind(this);
         tvTitle.setText("预约办事");
 
-        setTimes();//设置时间段
+        setTypes();//设置类型
+        //setTimes();//设置时间段
         popupwindowCalendarMonth.setText(popupwindowCalendar.getCalendarYear() + "年" + popupwindowCalendar.getCalendarMonth() + "月");
         setListeners();
 
+    }
+
+    private void setTypes() {
+        typeList.add("商品房现售备案");
+        typeList.add("建设工程招标备案");
+        typeList.add("新建建筑节能认可");
+        typeList.add("监理合同备案");
+        typeList.add("建筑企业养老保障金管理");
+        typeList.add("建设工程竣工结案备案");
+        typeList.add("建设工程招标控制价备案");
+
+
+        yybsTypeAdapter = new YybsTypeAdapter(getApplicationContext(), typeList);
+        lvType.setAdapter(yybsTypeAdapter);
     }
 
     private void setTimes() {
@@ -99,7 +135,7 @@ public class YybsActivity extends BaseActivity implements CompoundButton.OnCheck
 //        timeList.add("16:00-18:00");
 //        timeList.add("18:00-20:00");
 //
-//        yybsTimeAdapter = new YybsTimeAdapter2(getApplicationContext(), timeList);
+//        yybsTimeAdapter = new YybsTimeAdapter(getApplicationContext(), timeList);
 //        gvTime.setAdapter(yybsTimeAdapter);
 //        //gvTime.setAdapter(yybsTimeAdapter);
 //        //LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
@@ -174,6 +210,18 @@ public class YybsActivity extends BaseActivity implements CompoundButton.OnCheck
         rbTime21.setOnCheckedChangeListener(this);
         rbTime22.setOnCheckedChangeListener(this);
         rbTime23.setOnCheckedChangeListener(this);
+
+        lvType.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                rgTime1.clearCheck();
+                rgTime2.clearCheck();
+
+                etUserName.setText("");
+                etPhone.setText("");
+                etCode.setText("");
+            }
+        });
     }
 
     private void getData() {
@@ -182,7 +230,38 @@ public class YybsActivity extends BaseActivity implements CompoundButton.OnCheck
     }
 
 
-    @OnClick({R.id.iv_previousMonth, R.id.iv_pnextMonth})
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+        switch (compoundButton.getId()) {
+            case R.id.rb_time1_1:
+                rgTime2.clearCheck();
+                time = rbTime11.getText().toString();
+                break;
+            case R.id.rb_time1_2:
+                rgTime2.clearCheck();
+                time = rbTime12.getText().toString();
+                break;
+            case R.id.rb_time1_3:
+                rgTime2.clearCheck();
+                time = rbTime13.getText().toString();
+                break;
+            case R.id.rb_time2_1:
+                rgTime1.clearCheck();
+                time = rbTime21.getText().toString();
+                break;
+            case R.id.rb_time2_2:
+                rgTime1.clearCheck();
+                time = rbTime22.getText().toString();
+                break;
+            case R.id.rb_time2_3:
+                rgTime1.clearCheck();
+                time = rbTime23.getText().toString();
+                break;
+        }
+
+    }
+
+    @OnClick({R.id.iv_previousMonth, R.id.iv_pnextMonth, R.id.iv_more, R.id.btn_commit,R.id.btn_code})
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.iv_previousMonth://上个月
@@ -190,32 +269,61 @@ public class YybsActivity extends BaseActivity implements CompoundButton.OnCheck
                 break;
             case R.id.iv_pnextMonth://下个月
                 popupwindowCalendar.nextMonth();
-
+                break;
+            case R.id.iv_more:
+                moveListView();
+                break;
+            case R.id.btn_commit:
+                commit();
+                break;
+            case R.id.btn_code:
+                getCode();
                 break;
         }
     }
 
-    @Override
-    public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-        switch (compoundButton.getId()) {
-            case R.id.rb_time1_1:
-                rgTime2.clearCheck();
-                break;
-            case R.id.rb_time1_2:
-                rgTime2.clearCheck();
-                break;
-            case R.id.rb_time1_3:
-                rgTime2.clearCheck();
-                break;
-            case R.id.rb_time2_1:
-                rgTime1.clearCheck();
-                break;
-            case R.id.rb_time2_2:
-                rgTime1.clearCheck();
-                break;
-            case R.id.rb_time2_3:
-                rgTime1.clearCheck();
-                break;
+    private void getCode() {
+        if("".equals(etPhone.getText().toString())){
+            MyToast.showToast(getApplicationContext(),"电话不能为空");
+            return;
+        }else{
+            MyToast.showToast(getApplicationContext(),"正在获取验证码");
         }
     }
+
+    private void commit() {
+        if("".equals(date)||date==null){
+            MyToast.showToast(getApplicationContext(),"请选择日期");
+            return;
+        }
+        if("".equals(time)||time==null){
+            MyToast.showToast(getApplicationContext(),"请选择时间段");
+            return;
+        }
+        if("".equals(etUserName.getText().toString())){
+            MyToast.showToast(getApplicationContext(),"姓名不能为空");
+            return;
+        }
+        if("".equals(etCode.getText().toString())){
+            MyToast.showToast(getApplicationContext(),"验证码不能为空");
+            return;
+        }
+        if("".equals(etPhone.getText().toString())){
+            MyToast.showToast(getApplicationContext(),"电话不能为空");
+            return;
+        }
+        MyToast.showToast(getApplicationContext(),"预约成功");
+    }
+
+    private void moveListView() {
+        int i = lvType.getLastVisiblePosition();
+        if (i < typeList.size()) {
+            lvType.smoothScrollToPosition(i * 2 - 1);
+        } else {
+            lvType.smoothScrollToPosition(typeList.size());
+        }
+
+    }
+
+
 }
---
+++ app/src/main/java/com/eollse/adapter/YybsTypeAdapter.java
@@ -0,0 +1,40 @@
+package com.eollse.adapter;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.eollse.R;
+
+import java.util.List;
+
+/**
+ * Created by miliang on 2017/3/24/0024.
+ */
+
+public class YybsTypeAdapter extends MyBaseAdapter {
+    private List<String> list;
+    public YybsTypeAdapter(Context context, List list) {
+        super(context, list);
+        this.list=list;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        ViewHolder viewHolder;
+        if(view==null){
+            view=getLayoutInflater().inflate(R.layout.item_yybs_type,null);
+            viewHolder=new ViewHolder();
+            viewHolder.tv_type= (TextView) view.findViewById(R.id.tv_type);
+            view.setTag(viewHolder);
+        }
+        viewHolder= (ViewHolder) view.getTag();
+        viewHolder.tv_type.setText(list.get(i));
+        return view;
+    }
+
+    class ViewHolder{
+        TextView tv_type;
+    }
+}
