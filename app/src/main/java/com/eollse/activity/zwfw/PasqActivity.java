ce6d2b0a20122263717a747ac609c91f510f43e3
miliang <2318423730@qq.com>
2017-04-06 16:01:48 +0800

    2017-4-6 平安社区

--- app/src/main/java/com/eollse/activity/zwfw/PasqActivity.java
+++ app/src/main/java/com/eollse/activity/zwfw/PasqActivity.java
@@ -2,34 +2,58 @@ package com.eollse.activity.zwfw;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 import com.eollse.R;
 import com.eollse.activity.BaseActivity;
 import com.eollse.activity.MainActivity;
+import com.eollse.adapter.PasqAdapter;
 import com.eollse.app.MyApplication;
-import com.eollse.utils.SharedPreUtil;
+import com.eollse.utils.MyLeftLinearLayout;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 /**
  * 平安社区
  */
 public class PasqActivity extends BaseActivity {
 
+
+    @BindView(R.id.ll_myLeftLinearLayout)
+    MyLeftLinearLayout llMyLeftLinearLayout;
     @BindView(R.id.tv_backHome)
     TextView tvBackHome;
     @BindView(R.id.tv_title)
     TextView tvTitle;
     @BindView(R.id.tv_back)
     TextView tvBack;
-    @BindView(R.id.webView)
-    WebView webView;
+    @BindView(R.id.lv_info)
+    ListView lvInfo;
+    @BindView(R.id.iv_head)
+    ImageView ivHead;
+    @BindView(R.id.iv_img)
+    ImageView ivImg;
+    @BindView(R.id.ll_sqjk)
+    LinearLayout llSqjk;
+    @BindView(R.id.ll_ldrkgl)
+    LinearLayout llLdrkgl;
+    @BindView(R.id.ll_lwbj)
+    LinearLayout llLwbj;
+    @BindView(R.id.lv_right_info)
+    ListView lvRightInfo;
+    private PasqAdapter pasqAdapter;
+    private SimpleAdapter simpleAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,61 +61,83 @@ public class PasqActivity extends BaseActivity {
         setContentView(R.layout.activity_pasq);
         ButterKnife.bind(this);
 
+
         tvTitle.setText("平安社区");
         setListeners();
 
-        WebSettings settings = webView.getSettings();
-        settings.setBuiltInZoomControls(true);
-        settings.setSupportZoom(false);
-        settings.setJavaScriptEnabled(true);
-        settings.setDisplayZoomControls(false);
-        webView.setBackgroundColor(0);
-        //webView.setBackgroundResource(R.drawable.shape_info_background);
-        //webView.loadUrl("http://www.baidu.com");
-        String url="http://oa.ybqtw.org.cn/api/Html/ApiResoShow.aspx?TVInfoId=19&ResoId=29&Key=21218CCA77804D2BA1922C33E0151105&typeVer=";
-        webView.loadUrl("http://oa.ybqtw.org.cn/api/Html/ApiResoShow.aspx?TVInfoId="+ SharedPreUtil.getValue(this, "userinfo", "TVInfoId", "")+"&Key="+SharedPreUtil.getValue(this, "userinfo", "Key", "")+
-                "&ResoId=29");
-        webView.setWebViewClient(new WebViewClient(){
+//        WebSettings settings = webView.getSettings();
+//        settings.setBuiltInZoomControls(true);
+//        settings.setSupportZoom(false);
+//        settings.setJavaScriptEnabled(true);
+//        settings.setDisplayZoomControls(false);
+//        webView.setBackgroundColor(0);
+//        //webView.setBackgroundResource(R.drawable.shape_info_background);
+//        //webView.loadUrl("http://www.baidu.com");
+//        String url="http://oa.ybqtw.org.cn/api/Html/ApiResoShow.aspx?TVInfoId=19&ResoId=29&Key=21218CCA77804D2BA1922C33E0151105&typeVer=";
+//        webView.loadUrl("http://oa.ybqtw.org.cn/api/Html/ApiResoShow.aspx?TVInfoId="+ SharedPreUtil.getValue(this, "userinfo", "TVInfoId", "")+"&Key="+SharedPreUtil.getValue(this, "userinfo", "Key", "")+
+//                "&ResoId=29");
+//        webView.setWebViewClient(new WebViewClient(){
+//
+//            @Override
+//            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+//                Log.e("MyTAG",url);
+//                view.loadUrl(url);
+//                return true;
+//            }
+//
+//            @Override
+//            public void onPageFinished(WebView view, String url) {
+//
+//                super.onPageFinished(view, url);
+//            }
+//        });
+        List<HashMap<String,Object>> data = new ArrayList<HashMap<String,Object>>();
+        HashMap<String,Object> mapName = new HashMap<String,Object>();
+        mapName.put("info","姓名");
+        data.add(mapName);
 
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                Log.e("MyTAG",url);
-                view.loadUrl(url);
-                return true;
-            }
+        HashMap<String,Object> mapPhone = new HashMap<String,Object>();
+        mapPhone.put("info","电话");
+        data.add(mapPhone);
 
-            @Override
-            public void onPageFinished(WebView view, String url) {
+        HashMap<String,Object> mapJwsPhone = new HashMap<String,Object>();
+        mapJwsPhone.put("info","警务室电话");
+        data.add(mapJwsPhone);
 
-                super.onPageFinished(view, url);
-            }
-        });
+        HashMap<String,Object> mapBjPhone = new HashMap<String,Object>();
+        mapBjPhone.put("info","报警电话");
+        data.add(mapBjPhone);
+        simpleAdapter=new SimpleAdapter(PasqActivity.this,data,R.layout.item_pasq_right_info,new String[]{"info"},new int[]{R.id.tv_info});
+        lvRightInfo.setAdapter(simpleAdapter);
     }
 
     private void setListeners() {
-        //回首页
-        tvBackHome.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        llMyLeftLinearLayout.setBackZwfwActivity(this);
+    }
+
+
+    @OnClick({R.id.tv_backHome, R.id.tv_back, R.id.ll_sqjk, R.id.ll_ldrkgl, R.id.ll_lwbj})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.tv_backHome:
                 Intent intent = new Intent(PasqActivity.this, MainActivity.class);
                 startActivity(intent);
                 finish();
                 MyApplication.clearAllActivitiesWithOutMainActivity();
-            }
-        });
-        //返回
-        tvBack.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+ ;
+                break;
+            case R.id.tv_back:
+                finish();
+                break;
+            case R.id.ll_sqjk:
 
-                if(webView.canGoBack()) {
-                    webView.goBack();
-                }else {
-                    finish();
-                }
-            }
-        });
+                break;
+            case R.id.ll_ldrkgl:
+
+                break;
+            case R.id.ll_lwbj:
+
+                break;
+        }
     }
-
-
 }
---
+++ app/src/main/java/com/eollse/adapter/PasqAdapter.java
@@ -0,0 +1,39 @@
+package com.eollse.adapter;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.eollse.R;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2017/4/6/0006.
+ */
+
+public class PasqAdapter extends MyBaseAdapter {
+    private List<String> list;
+    public PasqAdapter(Context context, List list) {
+        super(context, list);
+        this.list=list;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder viewHolder;
+        if(convertView == null){
+            convertView=getLayoutInflater().inflate(R.layout.item_pasq_left,null);
+            viewHolder=new ViewHolder();
+            viewHolder.tv_info= (TextView) convertView.findViewById(R.id.tv_info);
+            convertView.setTag(viewHolder);
+        }
+        viewHolder= (ViewHolder) convertView.getTag();
+        viewHolder.tv_info.setText(list.get(position));
+        return convertView;
+    }
+    class ViewHolder{
+        TextView tv_info;
+    }
+}
